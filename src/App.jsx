14e2fdc84cf13193c6974c1f84b2b1279241354e
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2025-10-07 19:41:49 -0600

    se puede

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
     AOS.init({
       duration: 1000, // values from 0 to 3000, with step 50ms
       easing: 'ease-in-out', // default easing for AOS animations
-      once: true, // whether animation should happen only once - while scrolling down
+      // once: true, // whether animation should happen only once - while scrolling down
+      mirror: true,
     });
   }, []);
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import FeaturedProjects from '../components/FeaturedProjects';
+import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
 
 const LandingPage = () => {
@@ -10,6 +11,7 @@ const LandingPage = () => {
       <Navbar />
       <HeroSection />
       <FeaturedProjects />
+      <AboutUs />
       <Footer />
     </main>
   );
